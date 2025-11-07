46545ace79a7bdb7c53663381f3ca11535c6953d
OscarOrtiz05 <a01571904@tec.mx>
2025-11-06 18:31:23 -0600

    Archivos de texto que crea el codigo

--- Heap.h
+++ Heap.h
@@ -33,20 +33,23 @@ public:
 
 };
 
-/* un heap implementado sobre un arreglo de cualquier tipo de dato */
+/* un heap implementado sobre un arreglo de tipo Restaurante */
 class Heap
 {	public:
-    //	un arreglo de tipo T, tamano maximo y actual del heap
+    //	un arreglo de tipo Restaurante, tamaño máximo y actual del heap
     Restaurante *data;
     int maxSize;
     int currentSize;
 
+    //Constructor del Heap de tamaño de n
     Heap (int n)
     {	maxSize = n;
         currentSize = 0;
         data = new Restaurante[maxSize];
     }
 
+    //Destructor del Heap
+    //se tiene que especificar porque usa memoria dinámica
     ~Heap()
     {
         delete[] data;
@@ -63,11 +66,7 @@ class Heap
     int right(int);
 
     void push(string,int);
-    Restaurante pop();
-    void heapifyNombre(int);
     void swap(int, int);
-    void show();
-
     int search(string);
 
     void heapifyOrden(int);
@@ -81,108 +80,79 @@ class Heap
 
 };
 
+//Para saber si el Heap está vacío
+//Complejidad O(1)
 bool Heap::isEmpty()
 {	return currentSize == 0; }
 
+//Para saber si el Heap está lleno
+//Complejidad O(1)
 bool Heap::isFull()
 {	return currentSize == maxSize; }
 
 //	Regresa la raiz
+//Complejidad O(1)
 Restaurante Heap::getTop()
 {	return data[0]; }
 
+//Obtiene el padre del nodo
+//Complejidad O(1)
 int Heap::parent( int j)
 {	int p = (j - 1) / 2;
     return p;  	}
 
+//Obtiene el hijo izquierdo
+//Complejidad O(1)
 int Heap::left( int j)
 {	int l = 2*j + 1;
     return l;	}
 
+//Obtiene el hijo derecho
+//Complejidad O(1)
 int Heap::right( int j)
 {	int r = 2*j + 2;
     return r; 	}
 
+//Agrega un valor al heap
+//Complejidad O(log n)
 void Heap::push( string value, int n )
-{   if (!this->isFull())
-    {   int j =  currentSize;
+{
+    //Revisa que no esté lleno
+    if (!this->isFull())
+    {
+        //Inicializa los índices donde se
+        //comenzará el proceso de inserción
+        int j =  currentSize;
         int p = parent(j);
 
+        //Reacomoda los nodos para después insertar manteniendo la propiedad del Heap
         while (j > 0 && value < data[p].nombre)
         {
+            //Mueve el valor del índice p al índice j
             data[j] = data[p];
 
+            //Avanza j y p
             j = p;
             p = parent(j);
         }
 
+        //Inserta el nuevo valor
         data[j] = Restaurante(value, n);
         currentSize++;
 
     }else {
+        //Avisa cunando el Heap está lleno
         cout << "Heap lleno" << endl;
     }
 
 }
 
 //	intercanbia los valores de dos posiciones a y b
+//Complejidad O(1)
 void Heap::swap( int a, int b )
 {	Restaurante aux = data[a];
     data[a] = data[b];
     data[b] = aux;
-}
-
-void Heap::heapifyNombre( int index )
-{
-    int L = left(index), R = right(index), i = index;
-
-    if (L < currentSize && data[L].nombre > data[i].nombre) i=L;
-
-    if (R < currentSize && data[R].nombre > data[i].nombre) i=R;
-
-    if (i != index) {
-        swap(index, i);
-        heapifyNombre(i);
-    }
-
-}
-
-Restaurante Heap::pop( )
-{
-    if (!isEmpty()) {
-        //Remplaza el primer valor por el ultimo y decrementa el tamaño
-        Restaurante aux = data[0];
-        data[0] = data[currentSize-1];
-        currentSize--;
-
-        //Reacomoda la pila
-        heapifyNombre(0);
-
-        //Regresa el valor eliminado
-        return aux;
-    }
-
-    //Advierte que el heap está vacio
-    cout << "Heap vacio" << endl;
-    return Restaurante();
-
-}
-
-void Heap::show( )
-{	int i;
-    int level = 0;
-    cout << "Size: " << currentSize << endl ;
-
-    i = pow(2, level);
-    while( i-1 < currentSize)
-    {	cout << "\t" << data[i-1].nombre << "(Ordenes: " << data[i-1].totalOrdenes << ", Ingresos: " << data[i-1].totalIngreso << ")\n";
-        i++;
-        if ( (i) == pow(2, level+1) )
-        {
-            level++;
-        }
-    }
-    cout << endl ;
 }
 
 int Heap::search( string value ) {
